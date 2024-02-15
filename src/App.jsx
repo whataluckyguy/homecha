03fa71c6133343f1c3613b8487ca7674b18c2373
Lalit Kumar <67325910+whataluckyguy@users.noreply.github.com>
2024-02-15 16:20:05 +0530

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import React from "react";
 import "./App.css";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Typography,
+} from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 function App() {
   return (
-    <>
-      <div className="app">
-        <h1>Hello World</h1>
+    <div className="app">
+      <div className="navbar">
+        <Typography variant="h2" color={"white"}>
+          Deloitte
+        </Typography>
+        <Typography variant="h2" color="greenyellow">
+          .
+        </Typography>
       </div>
-    </>
+      <div className="main">
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+          <InputLabel htmlFor="search-with-icon">Type to search</InputLabel>
+          <Input
+            sx={{
+              borderRadius: "30px",
+            }}
+            id="outlined-adornment-search"
+            type="text"
+            startAdornment={
+              <InputAdornment position="start">
+                <IconButton aria-label="search">
+                  <Search />
+                </IconButton>
+              </InputAdornment>
+            }
+          ></Input>
+        </FormControl>
+      </div>
+    </div>
   );
 }
 
