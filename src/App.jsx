354cc91f3f4fb6b273d8aee00e0d4ef35d19d978
Lalit Kumar <67325910+whataluckyguy@users.noreply.github.com>
2024-02-15 19:15:30 +0530

    form err

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,129 @@
 import React from "react";
 import "./App.css";
 import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
   FormControl,
   IconButton,
+  Input,
   InputAdornment,
   InputLabel,
   OutlinedInput,
+  TextField,
   Typography,
 } from "@mui/material";
-import { Search } from "@mui/icons-material";
+import {
+  AccountCircle,
+  Article,
+  DriveEtaRounded,
+  Payment,
+  Payments,
+  Search,
+  Settings,
+} from "@mui/icons-material";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <div className="app">
       <div className="navbar">
+        <Box
+          sx={{
+            display: "flex",
+          }}
+        >
+          <Typography variant="h2" color={"white"}>
+            Deloitte
+          </Typography>
+          <Typography variant="h2" color="greenyellow">
+            .
+          </Typography>
+        </Box>
+
         <Typography variant="h2" color={"white"}>
-          Deloitte
-        </Typography>
-        <Typography variant="h2" color="greenyellow">
-          .
+          Login
         </Typography>
       </div>
       <div className="main">
-        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
-          <InputLabel htmlFor="search-with-icon">Type to search</InputLabel>
-          <Input
-            sx={{
-              borderRadius: "30px",
-            }}
-            id="outlined-adornment-search"
-            type="text"
-            startAdornment={
-              <InputAdornment position="start">
-                <IconButton aria-label="search">
-                  <Search />
-                </IconButton>
-              </InputAdornment>
-            }
-          ></Input>
-        </FormControl>
+        <Card
+          onClick={() => navigate("chat")}
+          variant="outlined"
+          sx={{
+            width: 300,
+            minHeight: 350,
+            border: "2px #ffbf00 solid",
+            borderRadius: "20px",
+            background: "rgb(230,229,230)",
+            background:
+              "radial-gradient(circle, rgba(230,229,230,1) 0%, rgba(5,5,6,1) 100%)",
+          }}
+        >
+          <div className="cardmedia">
+            <Payments sx={{ fontSize: 150, color: "white" }} />
+          </div>
+          <CardContent>
+            <Typography variant="h5" component={"div"}>
+              Cash Applications
+            </Typography>
+            <Typography variant="body2">
+              Cash application is a process of matching incoming payments with
+              open invoices in the accounts receivable department.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card
+          variant="outlined"
+          sx={{
+            width: 300,
+            border: "2px #ffbf00 solid",
+            borderRadius: "20px",
+            background: "rgb(230,229,230)",
+            background:
+              "radial-gradient(circle, rgba(230,229,230,1) 0%, rgba(5,5,6,1) 100%)",
+          }}
+        >
+          <div className="cardmedia">
+            <Article sx={{ fontSize: 150, color: "white" }} />
+          </div>
+          <CardContent>
+            <Typography gutterbottom variant="h5" component={"div"}>
+              Misc. Invoicing
+            </Typography>
+            <Typography variant="body2">
+              A miscellaneous invoice is an invoice created by the supplier for
+              items other than time sheets or expense sheets.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card
+          variant="outlined"
+          sx={{
+            width: 300,
+            border: "2px #ffbf00 solid",
+            borderRadius: "20px",
+            background: "rgb(230,229,230)",
+            background:
+              "radial-gradient(circle, rgba(230,229,230,1) 0%, rgba(5,5,6,1) 100%)",
+            // color: "white",
+          }}
+        >
+          <div className="cardmedia">
+            <Settings sx={{ fontSize: 150, color: "white" }} />
+          </div>
+          <CardContent>
+            <Typography gutterbottom variant="h5" component={"div"}>
+              Invoice Adjustment
+            </Typography>
+            <Typography variant="body2">
+              Invoice adjustments are a normal part of managing your accounts
+              receivables.
+            </Typography>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { StyledEngineProvider } from "@mui/material/styles";
+import Chat from "./Pages/Chat.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  { path: "/chat", element: <Chat /> },
   {
     path: "*",
     element: <h1>Page Not Found!</h1>,
@@ -17,6 +20,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* <StyledEngineProvider injectFirst> */}
     <RouterProvider router={router} />
+    {/* </StyledEngineProvider> */}
   </React.StrictMode>
 );
